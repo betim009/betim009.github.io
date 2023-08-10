bc9485394bfa33a0df3262e7b53dbafc646b922e
Alberto <betim009@gmail.com>
2023-08-10 00:14:59 -0300

    fix: refataroando

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import Header from './Header';
-import Home from './Home';
-import Form from './Form';
+
 
 function App() {
   return (
     <>
-      < Header />
-      < Home />
-      < Form />
+      Refatorando
     </>
   );
 }
